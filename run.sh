306e5227a4c275a817d3d5c08804c2e449c2d5e6
Enzo Brum <darosabrumenzo@gmail.com>
2024-04-30 17:35:59 -0300

    use python3 instead of python

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-python -m grpc_tools.protoc -I=./protos --python_out=./protos protos/*.proto --pyi_out=./protos --grpc_python_out=./protos
+python3 -m grpc_tools.protoc -I=./protos --python_out=./protos protos/*.proto --pyi_out=./protos --grpc_python_out=./protos
 docker compose build --parallel
 docker compose up
