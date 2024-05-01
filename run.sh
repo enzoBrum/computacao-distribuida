94cac90adf3f30c6f3cbe8ce924db314fd8c70fa
Enzo Brum <darosabrumenzo@gmail.com>
2024-05-01 19:24:08 -0300

    dont compile protos when using run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-python3 -m grpc_tools.protoc -I=./protos --python_out=./protos protos/*.proto --pyi_out=./protos --grpc_python_out=./protos
 docker compose build --parallel
 docker compose up
